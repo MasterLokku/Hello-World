5be16094b593332ec1b8be75496b084b8eba9224
Master_Lokku <lokku228@gmail.com>
2025-03-02 19:03:43 +0300

    Add general folder for solution
    * Nothing new

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public class Class1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(GetMinX(5, 2, 1));
+    }
+    private static string GetMinX(int a, int b, int c)
+    {
+        if (a == 0)
+        {
+            if (b == 0)
+            {
+                if (c == 0)
+                {
+                    return 0.ToString();
+                }
+                else
+                {
+                    if (b < c)
+                    {
+                        return b.ToString();
+                    }
+                    else
+                    {
+                        return c.ToString();
+                    }
+                }
+            }
+            else if (c == 0)
+            {
+                return "Impossible";
+            }
+            else
+            {
+                return "Impossible";
+            }
+        }
+        else
+        {
+            return ((-b) / 2 * a).ToString();
+        }
+    }
+}
